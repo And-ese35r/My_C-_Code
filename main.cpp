4d3fa5e9bb5074b16c8f0d14ed7b909a1a9c1dbe
And-ese35r <andsergeev1337@gmail.com>
2025-03-09 11:38:52 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\n\"Sun of the sleepless! Melancholy star!\"\nWhose tearful beam glows tremulously far,\nThat show\'st the darkness thou canst not dispel,\nHow like art thou to joy remember\'d well!" << endl;
+
+
+    return 0;
+}
